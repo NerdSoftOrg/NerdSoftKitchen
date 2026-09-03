72b46047332aab270f2fba32bd11b3be77a53ec7
bichal <vtorresnun@gmail.com>
2026-09-03 15:38:22 +0200

    Rebrand assets and configs to dice_and_delish
    Rename and move asset namespace from nerdsoftkitchen to dice_and_delish and update related resources. Add project LICENSE (AGPLv3 for code, CC BY-NC-SA for assets). Update service provider (IWailaPlugin) and mixins package to com.panzer.mods.dice_and_delish. Change pack.mcmeta translation key. Add new models/textures (finished_tortilla, skillet assets, bowl/rice textures, seeds, etc.), remove old nerdsoftkitchen model files, and relocate many textures/sounds to the new namespace. Adjust stonecutter build config to read required root properties (mod.id, mod.version) and update swaps/constants. Replace and reorganize stonecutter.properties.toml with new tool, dependency, and mod metadata for Dice & Delish.

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,12 +1,20 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("net.neoforged.moddev") version "2.0.143" apply false
+    id("net.neoforged.moddev") apply false
+}
+
+inline fun <reified T : Any> reqProperty(key: String): T {
+    return sc.properties.getOrNull<T>(key)
+        ?: error("Required root property '$key' is missing in stonecutter.properties.toml.")
 }
 
 stonecutter active "1.21.1"
 
+val modVersion: String = reqProperty("mod.version")
+val modId: String = reqProperty("mod.id")
+
 stonecutter parameters {
-    swaps["mod_version"] = "\"${properties.get<String>("mod_version")}\";"
+    swaps["mod_version"] = "\"$modVersion\";"
     swaps["minecraft"] = "\"${node.metadata.version}\";"
-    constants["release"] = properties.get<String>("mod_id") != "template"
+    constants["release"] = modId != "template"
 }
